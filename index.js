dad68120a13eb8d21dc13304255165a9d7a93b06
swakthika <swakthikachandran21@gmail.com>
2025-07-12 10:48:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var number=17;
+var name='js';
+var iscloudy=true;
+console.log(number+ " "+name+" "+iscloudy);
+document.write(number);
+document.write(name);
+document.write(iscloudy);
+//task2
+
+console.log(typeof(number));
+
+//task3
+var num1=10;
+var num2=15;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+
+//task4
+var num1=parseInt(prompt("enter first value: "));
+var num2=parseInt(prompt("enetr second value: "));
+alert(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
